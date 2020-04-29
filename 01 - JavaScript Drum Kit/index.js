f30ae83e12a6179909e8636b6971ac66f58b2f65
Tr1ckC0 <trick.co@yahoo.com>
2020-04-28 23:54:36 -0500

    day 4

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('keydown', bangTheDrum);
+
+    const keys = document.querySelectorAll(".key");
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+});
+
+function bangTheDrum() {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+};
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+
+    this.classList.remove('playing');
+};
