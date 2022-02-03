e5eee63b00fbeb5fe81f207171b25e9d0750d571
Tr1ckC0 <trick.co@yahoo.com>
2022-02-02 21:19:43 -0600

    finished

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -1,48 +1,54 @@
+const ENDPOINT = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const CITIES = [];
+
 document.addEventListener('DOMContentLoaded', () => {
-    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-    const cities = [];
+    getCities(ENDPOINT);
+    
     const searchInput = document.querySelector('.search');
     const suggestions = document.querySelector('.suggestions');
-
-
-    fetch(endpoint)
-        .then(resp => resp.json())
-        .then(json => cities.push(...json));
-
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', displayMatches);
-
-
-    // helper method
+    
+    searchInput.addEventListener('change', displayMatches)
+    searchInput.addEventListener('keyup', displayMatches)
 
     function displayMatches() {
-        const matchArray = findMatches(this.value, cities);
+        const matchArray = findMatchingCities(this.value);
         const html = matchArray.map(place => {
-            const regex = new RegExp(this.value, "gi");
+            const regex = new RegExp(this.value, 'gi');
             const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
             const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
             return `
-            <li>
-                <span class="name">${cityName}, ${stateName}</span>
-                <span class="population">${numberWithCommas(place.population)}</span>
-            </li>
-            `;
-        }).join('');
-        suggestions.innerHTML = html
-    };
+                <li>
+                    <span class="name">${cityName}, ${stateName}</span>
+                    <span class="population">${numberWithCommas(place.population)}</span>
+                </li>
+                `;
+            }).join('');
+            suggestions.innerHTML = html;
+        }
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        }
 });
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+function getCities(endpoint) {
+    fetch(endpoint)
+    .then(resp => resp.json())
+    .then(data => CITIES.push(...data));
 }
 
-function findMatches(wordToMatch, cityData) {
-    return cityData.filter(place => {
-        // figure if the city or state matches what was searched
+function findMatchingCities(wordToMatch) {
+    return CITIES.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex)
-    });
-};
+
+        return place.city.match(regex) || place.state.match(regex);
+    })
+}
+
+
+
+
+
 
 
 
