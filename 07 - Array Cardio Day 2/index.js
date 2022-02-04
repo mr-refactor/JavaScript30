7fa1e10354f24b85347b263f52f6a3ffa327c674
Tr1ckC0 <trick.co@yahoo.com>
2022-02-03 22:44:07 -0600

    finished

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -1,29 +1,48 @@
- // ## Array Cardio Day 2
+// ## Array Cardio Day 2
 
- const people = [
-    { name: 'Wes', year: 1988 },
-    { name: 'Kait', year: 1986 },
-    { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
-  ];
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
 
-  const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
 
-  // Some and Every Checks
-  // Array.prototype.some() // is at least one person 19 or older?
- 
-  // Array.prototype.every() // is everyone 19 or older?
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+function getAgeOf(person) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - person.year;
+}
 
-  // Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  // find the comment with the ID of 823423
+const isAdult = people.some((person) => getAgeOf(person) >= 19);
+console.log(isAdult);
 
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-  // delete the comment with the ID of 823423
+
+// Array.prototype.every() // is everyone 19 or older?
+const areAllAdults = people.every((person) => getAgeOf(person) >= 19);
+console.log(areAllAdults);
+
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const goodComment = comments.find((c) => c.id === 823423);
+console.log(goodComment);
+
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const indexOfGoodComment = comments.findIndex((c) => c.id === 823423);
+console.log(indexOfGoodComment);
+
